ce737f48cad8e4edb2aa29b0378179c825edc70b
Marica Odagaki <ento.entotto@gmail.com>
2016-06-30 07:53:15 -0700

    Group tests with describe

--- test/ch03_test.exs
+++ test/ch03_test.exs
@@ -3,79 +3,85 @@ defmodule Ch03Test do
   doctest Funpurr.Ch03
   alias Funpurr.Ch03.LeftistHeap, as: Heap
 
-  test "rank of an empty tree" do
-    assert Heap.rank(Heap.empty()) == 0
+  describe "Funpurr.Ch03.LeftistHeap.rank/1" do
+    test "rank of an empty tree" do
+      assert Heap.rank(Heap.empty()) == 0
+    end
+
+    test "rank of a non-empty tree" do
+      assert Heap.rank(leaf()) == 1
+    end
   end
 
-  test "rank of a non-empty tree" do
-    assert Heap.rank(leaf()) == 1
+  describe "Funpurr.Ch03.LeftistHeap.merge/2" do
+    test "merging a tree with an empty tree" do
+      tree = leaf()
+      assert Heap.merge(tree, Heap.empty()) == tree
+    end
+
+    test "merging an empty tree with a tree" do
+      tree = leaf()
+      assert Heap.merge(Heap.empty(), tree) == tree
+    end
+
+    test "merging h1 with h2" do
+      h1 = leaf('h1')
+      h2 = leaf('h2')
+      expected = %Heap.Tree{
+        rank: 1,
+        elem: 'h1',
+        left: h2,
+        right: Heap.empty(),
+      }
+      assert Heap.merge(h1, h2) == expected
+    end
+
+    test "merging h2 with h1" do
+      h1 = leaf('h1')
+      h2 = leaf('h2')
+      expected = %Heap.Tree{
+        rank: 1,
+        elem: 'h1',
+        left: h2,
+        right: Heap.empty(),
+      }
+      assert Heap.merge(h2, h1) == expected
+    end
   end
 
-  test "merging a tree with an empty tree" do
-    tree = leaf()
-    assert Heap.merge(tree, Heap.empty()) == tree
-  end
+  describe "Funpurr.Ch03.LeftistHeap.insert/2" do
+    test "insert h1 to empty" do
+      assert Heap.insert_through_merge('h1', Heap.empty()) == leaf('h1')
+      assert Heap.insert('h1', Heap.empty()) == leaf('h1')
+    end
 
-  test "merging an empty tree with a tree" do
-    tree = leaf()
-    assert Heap.merge(Heap.empty(), tree) == tree
-  end
+    test "insert h1 to h2" do
+      expected = %Heap.Tree{
+        rank: 1,
+        elem: 'h1',
+        left: leaf('h2'),
+        right: Heap.empty(),
+      }
+      assert Heap.insert_through_merge('h1', leaf('h2')) == expected
+      assert Heap.insert('h1', leaf('h2')) == expected
+    end
 
-  test "merging h1 with h2" do
-    h1 = leaf('h1')
-    h2 = leaf('h2')
-    expected = %Heap.Tree{
-      rank: 1,
-      elem: 'h1',
-      left: h2,
-      right: Heap.empty(),
-    }
-    assert Heap.merge(h1, h2) == expected
-  end
-
-  test "merging h2 with h1" do
-    h1 = leaf('h1')
-    h2 = leaf('h2')
-    expected = %Heap.Tree{
-      rank: 1,
-      elem: 'h1',
-      left: h2,
-      right: Heap.empty(),
-    }
-    assert Heap.merge(h2, h1) == expected
-  end
-
-  test "insert h1 to empty" do
-    assert Heap.insert_through_merge('h1', Heap.empty()) == leaf('h1')
-    assert Heap.insert('h1', Heap.empty()) == leaf('h1')
-  end
-
-  test "insert h1 to h2" do
-    expected = %Heap.Tree{
-      rank: 1,
-      elem: 'h1',
-      left: leaf('h2'),
-      right: Heap.empty(),
-    }
-    assert Heap.insert_through_merge('h1', leaf('h2')) == expected
-    assert Heap.insert('h1', leaf('h2')) == expected
-  end
-
-  test "insert h1 to h2-h3-h4" do
-    target = %Heap.Tree{
-      rank: 2,
-      elem: 'h2',
-      left: leaf('h3'),
-      right: leaf('h4'),
-    }
-    expected = %Heap.Tree{
-      rank: 1,
-      elem: 'h1',
-      left: target,
-      right: Heap.empty(),
-    }
-    assert Heap.insert_through_merge('h1', target) == expected
-    assert Heap.insert('h1', target) == expected
+    test "insert h1 to h2-h3-h4" do
+      target = %Heap.Tree{
+        rank: 2,
+        elem: 'h2',
+        left: leaf('h3'),
+        right: leaf('h4'),
+      }
+      expected = %Heap.Tree{
+        rank: 1,
+        elem: 'h1',
+        left: target,
+        right: Heap.empty(),
+      }
+      assert Heap.insert_through_merge('h1', target) == expected
+      assert Heap.insert('h1', target) == expected
+    end
   end
 
   defp leaf(elem \\ 'hello', rank \\ 1) do
