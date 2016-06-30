70b8989ed3228b2ac8cf47afa0cfc3776bd196f6
Marica Odagaki <ento.entotto@gmail.com>
2016-06-30 07:46:24 -0700

    Exercise 3.2

--- lib/ch03.ex
+++ lib/ch03.ex
@@ -19,5 +19,43 @@ defmodule Funpurr.Ch03 do
 
     def rank(%Empty{}), do: 0
     def rank(%Tree{rank: rank}), do: rank
+
+    def merge(h, %Empty{}), do: h
+    def merge(%Empty{}, h), do: h
+    def merge(
+      h1 = %Tree{elem: elem1, left: left1, right: right1},
+      h2 = %Tree{elem: elem2, left: left2, right: right2}) do
+      if elem1 <= elem2 do
+        make_tree(elem1, left1, merge(right1, h2))
+      else
+        make_tree(elem2, left2, merge(h1, right2))
+      end
+    end
+
+    def insert_through_merge(elem, h) do
+      merge(%Tree{rank: 1, elem: elem, left: empty(), right: empty()}, h)
+    end
+
+    # exercise 3.2
+    def insert(x, %Empty{}), do: make_leaf(1, x)
+    def insert(x, h = %Tree{elem: elem, left: left, right: right}) do
+      if x <= elem do
+        %Tree{rank: 1, elem: x, left: h, right: empty()}
+      else
+        make_tree(elem, left, insert(x, right))
+      end
+    end
+
+    defp make_tree(elem, left, right) do
+      if rank(left) >= rank(right) do
+        %Tree{rank: rank(right) + 1, elem: elem, left: left, right: right}
+      else
+        %Tree{rank: rank(left) + 1, elem: elem, left: right, right: left}
+      end
+    end
+
+    defp make_leaf(rank, elem) do
+      %Tree{rank: rank, elem: elem, left: empty(), right: empty()}
+    end
   end
 end
--- test/ch03_test.exs
+++ test/ch03_test.exs
@@ -8,11 +8,81 @@ defmodule Ch03Test do
   end
 
   test "rank of a non-empty tree" do
-    tree = %Heap.Tree{
+    assert Heap.rank(leaf()) == 1
+  end
+
+  test "merging a tree with an empty tree" do
+    tree = leaf()
+    assert Heap.merge(tree, Heap.empty()) == tree
+  end
+
+  test "merging an empty tree with a tree" do
+    tree = leaf()
+    assert Heap.merge(Heap.empty(), tree) == tree
+  end
+
+  test "merging h1 with h2" do
+    h1 = leaf('h1')
+    h2 = leaf('h2')
+    expected = %Heap.Tree{
       rank: 1,
-      elem: 'hello',
+      elem: 'h1',
+      left: h2,
+      right: Heap.empty(),
+    }
+    assert Heap.merge(h1, h2) == expected
+  end
+
+  test "merging h2 with h1" do
+    h1 = leaf('h1')
+    h2 = leaf('h2')
+    expected = %Heap.Tree{
+      rank: 1,
+      elem: 'h1',
+      left: h2,
+      right: Heap.empty(),
+    }
+    assert Heap.merge(h2, h1) == expected
+  end
+
+  test "insert h1 to empty" do
+    assert Heap.insert_through_merge('h1', Heap.empty()) == leaf('h1')
+    assert Heap.insert('h1', Heap.empty()) == leaf('h1')
+  end
+
+  test "insert h1 to h2" do
+    expected = %Heap.Tree{
+      rank: 1,
+      elem: 'h1',
+      left: leaf('h2'),
+      right: Heap.empty(),
+    }
+    assert Heap.insert_through_merge('h1', leaf('h2')) == expected
+    assert Heap.insert('h1', leaf('h2')) == expected
+  end
+
+  test "insert h1 to h2-h3-h4" do
+    target = %Heap.Tree{
+      rank: 2,
+      elem: 'h2',
+      left: leaf('h3'),
+      right: leaf('h4'),
+    }
+    expected = %Heap.Tree{
+      rank: 1,
+      elem: 'h1',
+      left: target,
+      right: Heap.empty(),
+    }
+    assert Heap.insert_through_merge('h1', target) == expected
+    assert Heap.insert('h1', target) == expected
+  end
+
+  defp leaf(elem \\ 'hello', rank \\ 1) do
+    %Heap.Tree{
+      rank: rank,
+      elem: elem,
       left: Heap.empty(),
       right: Heap.empty()}
-    assert Heap.rank(tree) == 1
   end
 end
